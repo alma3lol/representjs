e111741e6865c656d44a7987eccf1d62b20f00c2
Omar Almalol <omar.alma3lol@gmail.com>
2020-08-14 22:44:00 +0200

    feat(core): adds generic type 'T' on repository

--- src/core/repository.core.ts
+++ src/core/repository.core.ts
@@ -1,13 +1,13 @@
 import { Datasources, Core } from '..';
 
-export class Repository {
+export class Repository<T extends Core.Model<T>> {
 	constructor(
 		private _datasource: Datasources.Dexie
 	) { }
 	get datasource() { return this._datasource; }
 	find = (table: string) => this._datasource.find(table);
 	findById = (table: string, key: string | number) => this._datasource.findById(table, key);
-	create = <T extends Core.Model<T>>(table: string, model: T) => this._datasource.create(table, model);
+	create = (table: string, model: T) => this._datasource.create(table, model);
 	delete = (table: string, key: string) => this._datasource.delete(table, key);
-	update = <T extends Core.Model<T>>(table: string, model: T) => this._datasource.update(table, model);
+	update = (table: string, model: T) => this._datasource.update(table, model);
 }

7f8dd9f507830995b7d029293e30fa36ed42e455
Omar Almalol <omar.alma3lol@gmail.com>
2020-08-20 02:35:12 +0200

    feat(core): make 'Repository' class normal instead of generic

--- src/core/repository.core.ts
+++ src/core/repository.core.ts
@@ -1,13 +1,13 @@
 import { Datasources, Core } from '..';
 
-export class Repository<T extends Core.Model<T>> {
+export class Repository {
 	constructor(
 		private _datasource: Datasources.Dexie
 	) { }
 	get datasource() { return this._datasource; }
-	find = (table: string) => this._datasource.find<T>(table);
-	findById = (table: string, key: string | number) => this._datasource.findById<T>(table, key);
-	create = (table: string, model: T) => this._datasource.create(table, model);
-	delete = (table: string, key: string) => this._datasource.delete(table, key);
-	update = (table: string, model: T) => this._datasource.update(table, model);
+	find = <T extends Core.Model<T>>(table: string) => this._datasource.find<T>(table);
+	findById = <T extends Core.Model<T>>(table: string, key: string | number) => this._datasource.findById<T>(table, key);
+	create = <T extends Core.Model<T>>(table: string, model: T) => this._datasource.create(table, model);
+	delete = <T extends Core.Model<T>>(table: string, key: string) => this._datasource.delete(table, key);
+	update = <T extends Core.Model<T>>(table: string, model: T) => this._datasource.update(table, model);
 }

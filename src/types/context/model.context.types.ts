04e4e768703e9d626b7674a263cec46bac7771e0
Omar Almalol <omar.alma3lol@gmail.com>
2020-08-20 02:55:59 +0200

    feat(context): adds relations context key

--- src/types/context/model.context.types.ts
+++ src/types/context/model.context.types.ts
@@ -1,4 +1,4 @@
-import { Binding } from '../../Core';
+import { Binding, ORM } from '../../Core';
 
 export namespace Model {
 	/**
@@ -9,5 +9,6 @@ export namespace Model {
 		export const NAME_KEY = new Binding.Key<string>("MODEL.NAME");
 		export const URI_KEY = new Binding.Key<string>("MODEL.URI");
 		export const ID_PROPERTY_KEY = new Binding.Key<string>("MODEL.ID-PROPERTY");
+		export const RELATIONS_KEY = new Binding.Key<ORM.Relation[]>("MODEL.RELATION");
 	}
 }

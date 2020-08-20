d14ca9729461904e6260e1857118653b209bfe5d
Omar Almalol <omar.alma3lol@gmail.com>
2020-08-20 02:44:45 +0200

    feat(injectors): rewrite repository injector

--- src/injectors/repository.injector.ts
+++ src/injectors/repository.injector.ts
@@ -1,8 +1,15 @@
-import { Core, Types } from '..';
+import { Types, Datasources, Core } from '..';
+import { DexieOptions } from 'dexie';
 
 /**
- * Repositories injector
+ * Repository injector
  * 
- * @param cls Repository's class
+ * @param ds Repository's datasource
+ * @param config Datasource's config
+ * @param options Dexie datasource options
  */
-export const repository = (cls: Types.Common.Class<Core.Repository>) => Core.Injector.createClassInjector(cls);
+export const repository = (ds: Types.Common.Class<Datasources.Dexie>, config: Types.Datasources.Database.Config, options?: DexieOptions): PropertyDecorator => {
+	return (): PropertyDescriptor => ({
+		get: () => new Core.Repository(new ds(config, options))
+	})
+}

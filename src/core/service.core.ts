4c3951cec058675513f29be37325cf52ea7f0214
Omar Almalol <omar.alma3lol@gmail.com>
2020-08-20 02:44:01 +0200

    feat(core): move generic type on functions instead of class

--- src/core/service.core.ts
+++ src/core/service.core.ts
@@ -1,13 +1,13 @@
 import { Datasources, Core } from '..';
 
-export class Service<T extends Core.Model<T>> {
+export class Service {
 	constructor(
 		private _datasource: Datasources.API
 	) { }
 	get datasource() { return this._datasource; }
-	find = (uri: string, filter: any) => this._datasource.find<T>(uri, filter);
-	findById = (uri: string, id: string | number) => this._datasource.findById<T>(uri, id);
-	create = (uri: string, model: T) => this._datasource.create(uri, model);
-	delete = (uri: string, idOrFilter: any) => this._datasource.delete(uri, idOrFilter);
-	update = (uri: string, model: T, idOrFilter: any) => this._datasource.update(uri, model, idOrFilter);
+	find = <T extends Core.Model<T>>(uri: string, filter: any) => this._datasource.find<T>(uri, filter);
+	findById = <T extends Core.Model<T>>(uri: string, id: string | number) => this._datasource.findById<T>(uri, id);
+	create = <T extends Core.Model<T>>(uri: string, model: T) => this._datasource.create(uri, model);
+	delete = <T extends Core.Model<T>>(uri: string, idOrFilter: any) => this._datasource.delete(uri, idOrFilter);
+	update = <T extends Core.Model<T>>(uri: string, model: T, idOrFilter: any) => this._datasource.update(uri, model, idOrFilter);
 }

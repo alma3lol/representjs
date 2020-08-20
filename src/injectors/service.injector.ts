71bee899740584b49cd454885bd4caae301c31a9
Omar Almalol <omar.alma3lol@gmail.com>
2020-08-20 02:45:27 +0200

    feat(injectors): remove generic type from class initiation

--- src/injectors/service.injector.ts
+++ src/injectors/service.injector.ts
@@ -2,13 +2,13 @@ import { Core, Types, Booter, Context, Datasources } from '..';
 import { AxiosRequestConfig } from 'axios';
 
 /**
- * Services injector
+ * Service injector
  * 
  * @param ds Datasource's class
  * @param config AxiosRequestConfig to pass to the Datasources.API instance
  */
 export const service = (ds: Types.Common.Class<Datasources.API>, config?: AxiosRequestConfig): PropertyDecorator => {
 	return (): PropertyDescriptor => ({
-		get: () => new Core.Service<any>(new ds(config))
+		get: () => new Core.Service(new ds(config))
 	})
 }

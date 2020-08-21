e0a2eb035ae4b08625c73d5dcef93fb18169c4ca
Omar Almalol <omar.alma3lol@gmail.com>
2020-08-21 07:28:42 +0200

    feat(tests): recodes orm models mapper tests

--- src/tests/orm/mappers/model.test.ts
+++ src/tests/orm/mappers/model.test.ts
@@ -5,7 +5,7 @@ import { Model } from "../../../core";
 describe('ORM', () => {
 	describe('Mappers', () => {
 		describe('Model', () => {
-			it('should find a model using key & class', () => {
+			it('should find a model using keys & class', () => {
 				class Test extends Model<Test> {
 					something: string;
 				}
@@ -14,10 +14,26 @@ describe('ORM', () => {
 					something: "123"
 				});
 				mapper.bind<Test>("123").to(testClass);
-				const result = mapper.find("something", "123", Test);
+				const result = mapper.find({
+					something: "123"
+				}, Test);
 				expect(result).toBeInstanceOf(Test);
 				expect(result.something).toBe("123");
 				expect(result).toEqual(testClass);
+			});
+			it('should find models using iterator function & class', () => {
+				class Test extends Model<Test> {
+					something: string;
+				}
+				const mapper = Booter.getInstance().get(Mappers.Model.key());
+				mapper.bind("123").to(new Test({
+					something: "123"
+				}));
+				mapper.bind("321").to(new Test({
+					something: "321"
+				}));
+				const result = mapper.findMany(Test, _ => true);
+				expect(result.length).toBe(2);
 			});
 		});
 	});

0665e28b09269734ff93aee2c529d2decf02d3fe
Omar Almalol <omar.alma3lol@gmail.com>
2020-08-21 04:43:47 +0200

    chore(injectors): corrects dexie datasource's config path

--- src/injectors/repository.injector.ts
+++ src/injectors/repository.injector.ts
@@ -8,7 +8,7 @@ import { DexieOptions } from 'dexie';
  * @param config Datasource's config
  * @param options Dexie datasource options
  */
-export const repository = (ds: Types.Common.Class<Datasources.Dexie>, config: Types.Datasources.Database.Config, options?: DexieOptions): PropertyDecorator => {
+export const repository = (ds: Types.Common.Class<Datasources.Dexie>, config: Types.Datasources.Dexie.Config, options?: DexieOptions): PropertyDecorator => {
 	return (): PropertyDescriptor => ({
 		get: () => new Core.Repository(new ds(config, options))
 	})

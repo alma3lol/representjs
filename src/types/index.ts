b2db1772b4e8cf59583ac77f17d174b639603e1a
Omar Almalol <omar.alma3lol@gmail.com>
2020-08-21 05:08:31 +0200

    chore(types): corrects imports

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,5 @@
-export * as Booter from './booter.types';
 export * as Common from './common.types';
-export * as Context from './context';
 export * as Core from './core';
 export * as Datasources from './datasources';
-export * as Injectors from './injectors';
+
+export * as Bindings from './bindings';

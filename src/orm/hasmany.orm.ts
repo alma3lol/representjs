b6af3180b55f7bfe5aacdc70d7dd9b724420378e
Omar Almalol <omar.alma3lol@gmail.com>
2020-08-21 07:30:34 +0200

    chore(orm): adjusts code to meet config & functions requirements

--- src/orm/hasmany.orm.ts
+++ src/orm/hasmany.orm.ts
@@ -4,11 +4,7 @@ import { ORM } from '../core';
 export class HasMany<T extends Core.Model<T>> extends ORM.Relation implements Types.Core.ORM.ManyModelsRelation<T> {
 	type: Types.Core.ORM.RelationType = "HasMany";
 	constructor(
-		protected readonly config: Types.Core.ORM.RelationConfig<T>
+		protected readonly config: Types.Core.ORM.ManyModelsRelationConfig<T>
 	) { super(config); }
-	toModels = () => {
-		return this.mapper.findMany(this.config.cls, (model) => {
-			return false;
-		})
-	}
+	toModels = () => this.mapper.findMany(this.config.cls, this.config.iteratorFn)
 }
--- src/orm/hasone.orm.ts
+++ src/orm/hasone.orm.ts
@@ -4,7 +4,7 @@ import { ORM, Model } from '../core';
 export class HasOne<T extends Model<T>> extends ORM.Relation implements Types.Core.ORM.OneModelRelation<T> {
 	type: Types.Core.ORM.RelationType = "HasOne";
 	constructor(
-		protected readonly config: Types.Core.ORM.RelationConfig<T>
+		protected readonly config: Types.Core.ORM.OneModelRelationConfig<T>
 	) { super(config); }
-	toModel = () => this.mapper.find(this.config.key, this.config.cls);
+	toModel = () => this.mapper.find(this.config.keys, this.config.cls);
 }

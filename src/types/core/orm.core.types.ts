0ba1ae7d89b8648937538f6757232ee98f2e2a95
Omar Almalol <omar.alma3lol@gmail.com>
2020-08-21 05:08:09 +0200

    feat(types): adds ORM's relation types & metadata type

--- src/types/core/orm.core.types.ts
+++ src/types/core/orm.core.types.ts
@@ -1,6 +1,42 @@
+import { Class } from '../common.types';
+import { Model, ORM as CoreORM } from "../../core";
+
 export namespace ORM {
 	/**
 	 * ORM relation type
 	 */
 	export type RelationType = "HasMany" | "HasOne" | "BelongsTo";
+	/**
+	 * ORM relation config
+	 */
+	export type RelationConfig<T extends Model<T>> = {
+		/**
+		 * Relation's name
+		 */
+		name?: string
+		/**
+		 * Foriegn key
+		 */
+		key: string
+		/**
+		 * Model's class
+		 */
+		cls: Class<T>
+	}
+	/**
+	 * Relations metadata type
+	 */
+	export type MetadataType = Map<string, CoreORM.Relation>
+	/**
+	 * One object model relation interface
+	 */
+	export interface OneModelRelation<T extends Model<T>> {
+		toModel: () => T | undefined;
+	}
+	/**
+	 * Many object models relation interface
+	 */
+	export interface ManyModelsRelation<T extends Model<T>> {
+		toModels: () => T[];
+	}
 }

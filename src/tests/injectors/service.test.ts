4c1ae6ec4b81f97663df86b7b55ea2c9dbdcbfdd
Omar Almalol <omar.alma3lol@gmail.com>
2020-08-21 05:10:51 +0200

    chore(tests): removes generic type on service from tests

--- src/tests/injectors/service.test.ts
+++ src/tests/injectors/service.test.ts
@@ -5,10 +5,9 @@ describe('Injectors', () => {
 	describe('Service', () => {
 		it('should inject a service into a property', () => {
 			class TestDS extends Datasources.API { }
-			class TestModel extends Core.Model<TestModel> { }
 			class Test {
 				@Injectors.service(TestDS)
-				service: Core.Service<TestModel>;
+				service: Core.Service;
 			}
 			const testClass = new Test();
 			expect(testClass.service).toBeInstanceOf(Core.Service);

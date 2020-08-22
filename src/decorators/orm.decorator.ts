a172d88a67dfde80929b5d83f96e4410cada0c12
Omar Almalol <omar.alma3lol@gmail.com>
2020-08-22 08:17:39 +0200

    feat(decorators):
    - Changes relations config to match the unified type
    - Generic types on getRelationsFromTarget/setRelationsOnTarget
    - Adds auto-resolve values for relations

--- src/decorators/orm.decorator.ts
+++ src/decorators/orm.decorator.ts
@@ -2,53 +2,65 @@ import { Types, Utils, Core } from '..';
 import { HasOne, HasMany, BelongsTo } from "../orm";
 
 export namespace ORM {
-	export const relation = <T extends Core.Model<T>>(type: Types.Core.ORM.RelationType, config: Types.Core.ORM.ManyModelsRelationConfig<T> | Types.Core.ORM.OneModelRelationConfig<T>): PropertyDecorator => {
+	export const relation = <T extends Core.Model<T>>(type: Types.Core.ORM.RelationType, config: Types.Core.ORM.RelationConfig<T>): PropertyDecorator => {
 		switch (type) {
 			case "HasOne":
-				return Relation.hasOne(config as Types.Core.ORM.OneModelRelationConfig<T>);
+				return Relation.hasOne(config);
 			case "HasMany":
-				return Relation.hasMany(config as Types.Core.ORM.ManyModelsRelationConfig<T>);
+				return Relation.hasMany(config);
 			case "BelongsTo":
-				return Relation.belongsTo(config as Types.Core.ORM.OneModelRelationConfig<T>);
+				return Relation.belongsTo(config);
 			default:
 				throw new Error(`No such a relation: '${type}'`);
 		}
 	}
 	export namespace Relation {
-		const getRelationsFromTarget = (target: Object): Types.Core.ORM.MetadataType => {
-			let relations: Types.Core.ORM.MetadataType = Utils.Reflector.getMetadata(Types.Bindings.Model.RELATIONS_KEY.toString(), target);
+		const getRelationsFromTarget = <T extends Core.Model<T>>(target: Object): Types.Core.ORM.MetadataType<T> => {
+			let relations: Types.Core.ORM.MetadataType<T> = Utils.Reflector.getMetadata(Types.Bindings.Model.RELATIONS_KEY.toString(), target);
 			if (relations === undefined) relations = new Map();
 			return relations;
 		}
-		const setRelationsOnTarget = (target: Object, relations: Types.Core.ORM.MetadataType) => Utils.Reflector.defineMetadata(Types.Bindings.Model.RELATIONS_KEY.toString(), relations, target);
-		export const hasOne = <T extends Core.Model<T>>(config: Types.Core.ORM.OneModelRelationConfig<T>): PropertyDecorator => {
+		const setRelationsOnTarget = <T extends Core.Model<T>>(target: Object, relations: Types.Core.ORM.MetadataType<T>) => Utils.Reflector.defineMetadata(Types.Bindings.Model.RELATIONS_KEY.toString(), relations, target);
+		export const hasOne = <T extends Core.Model<T>>(config: Types.Core.ORM.RelationConfig<T>): PropertyDecorator => {
 			return (
 				target,
 				propertyKey
-			) => {
+			): PropertyDescriptor => {
+				const relation = new HasOne(config);
 				const relations = getRelationsFromTarget(target);
-				relations.set(propertyKey.toString(), new HasOne(config));
+				relations.set(propertyKey.toString(), relation);
 				setRelationsOnTarget(target, relations);
+				return {
+					get: () => relation.toModel(target as T)
+				}
 			}
 		}
-		export const hasMany = <T extends Core.Model<T>>(config: Types.Core.ORM.ManyModelsRelationConfig<T>): PropertyDecorator => {
+		export const hasMany = <T extends Core.Model<T>>(config: Types.Core.ORM.RelationConfig<T>): PropertyDecorator => {
 			return (
 				target,
 				propertyKey
-			) => {
+			): PropertyDescriptor => {
+				const relation = new HasMany(config);
 				const relations = getRelationsFromTarget(target);
-				relations.set(propertyKey.toString(), new HasMany(config));
+				relations.set(propertyKey.toString(), relation);
 				setRelationsOnTarget(target, relations);
+				return {
+					get: () => relation.toModels(target as T)
+				}
 			}
 		}
-		export const belongsTo = <T extends Core.Model<T>>(config: Types.Core.ORM.OneModelRelationConfig<T>): PropertyDecorator => {
+		export const belongsTo = <T extends Core.Model<T>>(config: Types.Core.ORM.RelationConfig<T>): PropertyDecorator => {
 			return (
 				target,
 				propertyKey
-			) => {
+			): PropertyDescriptor => {
+				const relation = new BelongsTo(config);
 				const relations = getRelationsFromTarget(target);
-				relations.set(propertyKey.toString(), new BelongsTo(config));
+				relations.set(propertyKey.toString(), relation);
 				setRelationsOnTarget(target, relations);
+				return {
+					get: () => relation.toModel(target as T)
+				}
 			}
 		}
 	}

f81d3b6423db0b6fe3c7e13c8bf3a4a525b6431d
Omar Almalol <omar.alma3lol@gmail.com>
2020-08-22 08:25:45 +0200

    fix(types): adds missing generic type on relations

--- src/types/bindings/model.bindings.types.ts
+++ src/types/bindings/model.bindings.types.ts
@@ -5,5 +5,5 @@ export namespace Model {
 	export const NAME_KEY = new Binding.Key<string>("MODEL.NAME");
 	export const URI_KEY = new Binding.Key<string>("MODEL.URI");
 	export const ID_PROPERTY_KEY = new Binding.Key<string>("MODEL.ID-PROPERTY");
-	export const RELATIONS_KEY = new Binding.Key<ORM.Relation[]>("MODEL.RELATION");
+	export const RELATIONS_KEY = new Binding.Key<ORM.Relation<any>[]>("MODEL.RELATION");
 }

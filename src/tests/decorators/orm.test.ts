819e67d3ac01f7b3e069bf2a1cddc12d1cbb0ee5
Omar Almalol <omar.alma3lol@gmail.com>
2020-08-22 08:20:31 +0200

    chore(tests): refactors tests for orm

--- src/tests/decorators/orm.test.ts
+++ src/tests/decorators/orm.test.ts
@@ -1,21 +1,25 @@
 import { Decorators, Core, Utils, Types, Booter, ORM } from "../..";
+import { HasMany } from '../../orm';
 
 describe('Decorators', () => {
 	describe('ORM', () => {
 		class TestModel extends Core.Model<TestModel> {
-			test: string;
+			id: string;
 		}
 		beforeAll(() => {
 			const mapper = Booter.getInstance().get(Types.Bindings.Core.MODEL_MAPPER_KEY);
-			mapper.bind("123").to(new TestModel({ test: "123" }));
-			mapper.bind("321").to(new TestModel({ test: "321" }));
+			mapper.bind("123").to(new TestModel({ id: "123" }));
 		})
 		it('should inject a relation into model\'s metadata', () => {
-			class Test {
+			class Test extends Core.Model<TestModel> {
+				@Decorators.Model.property({
+					id: true,
+					default: "123"
+				})
+				id: string;
+
 				@Decorators.ORM.relation("HasOne", {
-					keys: {
-						test: "123"
-					},
+					key: "id",
 					cls: TestModel
 				})
 				test: TestModel;
@@ -24,18 +28,23 @@ describe('Decorators', () => {
 			expect(result).toBeInstanceOf(Map);
 			expect(result.get("test")).toBeInstanceOf(ORM.HasOne);
 		});
-		it('should inject a HasMany relation into model\'s metadata', () => {
-			class Test {
-				@Decorators.ORM.Relation.hasMany({
-					cls: TestModel,
-					iteratorFn: _ => true
+		it('should auto-resolve relation\'s value', () => {
+			class Test extends Core.Model<TestModel> {
+				@Decorators.Model.property({
+					id: true,
+					default: "123"
+				})
+				id: string;
+
+				@Decorators.ORM.relation("HasOne", {
+					key: "id",
+					cls: TestModel
 				})
 				test: TestModel;
 			}
-			const result = Utils.Reflector.getMetadata(Types.Bindings.Model.RELATIONS_KEY.toString(), new Test())
-			expect(result).toBeInstanceOf(Map);
-			expect(result.get("test")).toBeInstanceOf(ORM.HasMany);
-			expect(result.get("test").toModels().length).toBe(2);
+			const testClass = new Test();
+			expect(testClass.test).toBeInstanceOf(TestModel);
+			expect(testClass.test.id).toBe("123");
 		});
 	});
 });

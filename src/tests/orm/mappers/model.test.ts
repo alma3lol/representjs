d15e18fdf1762c5f53bbf58aed818791b8c20bc9
Omar Almalol <omar.alma3lol@gmail.com>
2020-08-22 08:21:46 +0200

    chore(tests): refactors models mapper testing code for better results

--- src/tests/orm/mappers/model.test.ts
+++ src/tests/orm/mappers/model.test.ts
@@ -5,34 +5,34 @@ import { Model } from "../../../core";
 describe('ORM', () => {
 	describe('Mappers', () => {
 		describe('Model', () => {
+			const mapper = Booter.getInstance().get(Mappers.Model.key());
+			afterEach(() => {
+				mapper.clear();
+			})
 			it('should find a model using keys & class', () => {
 				class Test extends Model<Test> {
 					something: string;
 				}
-				const mapper = Booter.getInstance().get(Mappers.Model.key());
 				const testClass = new Test({
 					something: "123"
 				});
 				mapper.bind<Test>("123").to(testClass);
-				const result = mapper.find({
-					something: "123"
-				}, Test);
+				const result = mapper.find(Test, "something", "123");
 				expect(result).toBeInstanceOf(Test);
-				expect(result.something).toBe("123");
+				expect(result?.something).toBe("123");
 				expect(result).toEqual(testClass);
 			});
 			it('should find models using iterator function & class', () => {
 				class Test extends Model<Test> {
 					something: string;
 				}
-				const mapper = Booter.getInstance().get(Mappers.Model.key());
 				mapper.bind("123").to(new Test({
 					something: "123"
 				}));
 				mapper.bind("321").to(new Test({
-					something: "321"
+					something: "123"
 				}));
-				const result = mapper.findMany(Test, _ => true);
+				const result = mapper.findMany(Test, "something", "123");
 				expect(result.length).toBe(2);
 			});
 		});

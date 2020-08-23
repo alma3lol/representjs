ad29cc7e749c494adb66f8420bad7865aa043bf9
Omar Almalol <omar.alma3lol@gmail.com>
2020-08-23 16:30:43 +0200

    feat(decorators):
    - Replaces reflector usage with model's registery binding

--- src/decorators/model.decorator.ts
+++ src/decorators/model.decorator.ts
@@ -1,4 +1,4 @@
-import { Utils, Types } from '..';
+import { Types, Core, Booter } from '..';
 import { v4 as uuidv4 } from "uuid";
 
 /**
@@ -6,7 +6,7 @@ import { v4 as uuidv4 } from "uuid";
  * 
  * @param config Model's configuration
  */
-export const model = (config?: Types.Core.Model.Config): ClassDecorator => Model.config(config);
+export const model = <T extends Core.Model<T>>(config: Types.Core.Model.Config) => Model.config(config);
 
 /**
  * A namespaced model decorators
@@ -17,17 +17,32 @@ export namespace Model {
 	 * 
 	 * @param config Model's configuration
 	 */
-	export const config = (config?: Types.Core.Model.Config): ClassDecorator => {
+	export const config = <T extends Core.Model<T>>(config: Types.Core.Model.Config) => {
 		return (
-			target
+			target: Types.Common.Class<T>
 		) => {
-			if (config !== undefined) {
-				if (config.table !== undefined)
-					Utils.Reflector.defineMetadata(Types.Bindings.Model.TABLE_KEY.toString(), config.table, target);
-				if (config.name !== undefined)
-					Utils.Reflector.defineMetadata(Types.Bindings.Model.NAME_KEY.toString(), config.name, target);
-				if (config.uri !== undefined)
-					Utils.Reflector.defineMetadata(Types.Bindings.Model.URI_KEY.toString(), config.uri, target);
+			Core.Model.bind(target.name, Types.Bindings.Model.TABLE_KEY).to(config.table);
+			Core.Model.bind(target.name, Types.Bindings.Model.URI_KEY).to(config.uri);
+			const models = Booter.getInstance().get(Types.Bindings.Core.MODELS_KEY);
+			models.push(target);
+			Booter.getInstance().bind(Types.Bindings.Core.MODELS_KEY).to(models);
+		}
+	}
+
+	export const ID = (config: Types.Core.Model.PropertyConfig & { type: Function }): PropertyDecorator => {
+		return (
+			target,
+			propertyKey
+		) => {
+			Core.Model.bind(target.constructor.name, Types.Bindings.Model.ID_PROPERTY_KEY).to(propertyKey.toString());
+			Core.Model.bind(target.constructor.name, Types.Bindings.Model.ID_PROPERTY_TYPE_KEY).to(config.type);
+			if (config.defaultFn !== undefined || config.default !== undefined) {
+				let val = (config.default !== undefined) ? config.default : uuidv4();
+				const descriptor: PropertyDescriptor = {
+					get: () => val,
+					set: (v) => val = v
+				}
+				return descriptor;
 			}
 		}
 	}
@@ -37,24 +52,24 @@ export namespace Model {
 	 * 
 	 * @param config Property configuration
 	 */
-	export const property = (config?: Types.Core.Model.PropertyConfig): PropertyDecorator => {
+	export const property = <T extends Core.Model<T>>(config?: Types.Core.Model.PropertyConfig) => {
 		return (
-			target,
-			propertyKey
+			target: T,
+			propertyKey: string
 		): any => {
 			if (config !== undefined) {
-				if (config.id !== undefined && config.id === true)
-					Utils.Reflector.defineMetadata(Types.Bindings.Model.ID_PROPERTY_KEY.toString(), propertyKey, target)
 				if (config.defaultFn !== undefined || config.default !== undefined) {
 					let val = (config.default !== undefined) ? config.default : uuidv4();
 					const descriptor: PropertyDescriptor = {
-						configurable: true,
-						enumerable: true,
 						get: () => val,
 						set: (v) => val = v
 					}
-					return descriptor
+					return descriptor;
 				}
+			} else {
+				const properties: string[] = Core.Model.getSubKey(target.Name, Types.Bindings.Model.PROPERTIES_KEY)?.value ?? [];
+				if (!properties.includes(propertyKey.toString())) properties.push(propertyKey.toString());
+				Core.Model.bind(target.Name, Types.Bindings.Model.PROPERTIES_KEY).to(properties);
 			}
 		}
 	}

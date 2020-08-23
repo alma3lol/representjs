7d9104074b0c42cfc92fde7c0ee8dd41086b1988
Omar Almalol <omar.alma3lol@gmail.com>
2020-08-23 07:06:32 +0200

    feat(datasources): adds models mapper bind/unbind on chane

--- src/datasources/api.datasource.ts
+++ src/datasources/api.datasource.ts
@@ -1,4 +1,4 @@
-import { Core } from '..';
+import { Core, Types } from '..';
 import Axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 
 export class API extends Core.Datasource {
@@ -8,19 +8,45 @@ export class API extends Core.Datasource {
 		this._client = Axios.create(config);
 	}
 	get client() { return this._client; }
-	async find<T extends Core.Model<T>>(uri: string, filter: any): Promise<T[]> {
-		return await this._client.get(`${uri}/?filter=${JSON.stringify(filter)}`).then(res => res.data) as T[];
+	async find<T extends Core.Model<T>>(uri: string, filter: Types.Thirdparty.Filter<T>): Promise<T[]> {
+		const models = await this._client.get(`${uri}/?filter=${JSON.stringify(filter)}`).then(res => res.data) as T[];
+		models.forEach(model => this.mapper.bind<T>(model.ID).to(model));
+		return models;
 	}
-	async findById<T extends Core.Model<T>>(uri: string, id: string | number): Promise<T> {
-		return await this._client.get(`${uri}/${id}`).then(res => res.data) as T;
+	async findById<T extends Core.Model<T>>(uri: string, id: string | number): Promise<T | undefined> {
+		const model = await this._client.get(`${uri}/${id}`).then(res => res.data) as T | undefined;
+		if (model !== undefined) this.mapper.bind<T>(model.ID).to(model);
+		return model;
 	}
 	async create<T extends Core.Model<T>>(uri: string, model: T): Promise<T> {
-		return await this._client.post(`${uri}/`, model.toJSON()).then(res => res.data) as T;
+		const returnModel = await this._client.post(`${uri}/`, model.toJSON()).then(res => res.data) as T;
+		this.mapper.bind<T>(returnModel.ID).to(returnModel);
+		return returnModel;
 	}
-	async delete(uri: string, idOrFilter: any): Promise<boolean> {
-		return await this._client.delete(`${uri}${(typeof idOrFilter === "string") ? `/${idOrFilter}` : `?where=${JSON.stringify(idOrFilter)}`}`).then(res => res.status === 200);
+	async delete<T extends Core.Model<T>>(uri: string, idOrFilter: string | Types.Thirdparty.Where<T>): Promise<boolean> {
+		const deleted = await this._client.delete(`${uri}${(typeof idOrFilter === "string") ? `/${idOrFilter}` : `?where=${JSON.stringify(idOrFilter)}`}`).then(res => res.status === 200);
+		if (deleted) {
+			const keysToDeleted: string[] = [];
+			if (typeof idOrFilter === "string") keysToDeleted.push(idOrFilter);
+			else Object.keys(idOrFilter).forEach(key => keysToDeleted.push(key));
+			keysToDeleted.forEach(key => this.mapper.unbind(key));
+		}
+		return deleted;
 	}
-	async update<T extends Core.Model<T>>(uri: string, model: T, idOrFilter: any): Promise<number> {
-		return await this._client.put(`${uri}${(typeof idOrFilter === "string") ? `/${idOrFilter}` : `?where=${JSON.stringify(idOrFilter)}`}`, model.toJSON()).then(res => res.data.count) as number;
+	async update<T extends Core.Model<T>>(uri: string, model: T, idOrFilter: string | Types.Thirdparty.Where<T>): Promise<number> {
+		const count = await this._client.put(`${uri}${(typeof idOrFilter === "string") ? `/${idOrFilter}` : `?where=${JSON.stringify(idOrFilter)}`}`, model.toJSON()).then(res => res.data.count) as number;
+		if (count) {
+			const keysToUpdate: string[] = [];
+			if (typeof idOrFilter === "string") keysToUpdate.push(idOrFilter);
+			else Object.keys(idOrFilter).forEach(key => keysToUpdate.push(key))
+			keysToUpdate.forEach(key => {
+				const mappedModel = this.mapper.get<T>(key) as any;
+				Object.keys(model).forEach(property => {
+					if (model[property] !== undefined) mappedModel[property] = model[property];
+				});
+				this.mapper.bind<T>(key).to(mappedModel);
+			});
+		}
+		return count;
 	}
 }
--- src/datasources/dexie.datasource.ts
+++ src/datasources/dexie.datasource.ts
@@ -11,18 +11,34 @@ export class Dexie extends Core.Datasource {
 	get connection() { return this._connection; }
 	open = () => this._connection.open();
 	async find<T extends Core.Model<T>>(table: string): Promise<T[]> {
-		return this._connection.table<T>(table).toArray();
+		const models = await this._connection.table<T>(table).toArray();
+		models.forEach(model => this.mapper.bind<T>(model.ID).to(model));
+		return models;
 	}
-	async findById<T extends Core.Model<T>>(table: string, key: string | number): Promise<T> {
-		return await this._connection.table<T>(table).get(key).then(v => v) as T;
+	async findById<T extends Core.Model<T>>(table: string, key: string | number): Promise<T | undefined> {
+		const model = await this._connection.table<T>(table).get(key).then(v => v) as T | undefined;
+		if (model !== undefined) this.mapper.bind<T>(model.ID).to(model);
+		return model;
 	}
 	async create<T extends Core.Model<T>>(table: string, model: T): Promise<T> {
-		return await this._connection.table<T>(table).add(model, model.ID).then(() => model);
+		await this._connection.table<T>(table).add(model, model.ID);
+		this.mapper.bind<T>(model.ID).to(model);
+		return model;
 	}
 	async delete(table: string, key: string): Promise<boolean> {
-		return await this._connection.table(table).delete(key).then(() => true, () => false);
+		const deleted = await this._connection.table(table).delete(key).then(() => true, () => false);
+		if (deleted) this.mapper.unbind(key);
+		return deleted;
 	}
 	async update<T extends Core.Model<T>>(table: string, model: T): Promise<number> {
-		return await this._connection.table<T>(table).update(model.ID, model).then(v => v);
+		const count = await this._connection.table<T>(table).update(model.ID, model).then(v => v);
+		if (count) {
+			const mappedModel = this.mapper.get<T>(model.ID) as any;
+			Object.keys(model).forEach(property => {
+				if (model[property] !== undefined) mappedModel[property] = model[property];
+			});
+			this.mapper.bind<T>(model.ID).to(mappedModel);
+		}
+		return count;
 	}
 }

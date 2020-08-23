1117bddcd288f77e16a6d077f5cf55430df7ca72
Omar Almalol <omar.alma3lol@gmail.com>
2020-08-23 07:05:48 +0200

    feat(core): adds loopback filter to core datasource class functions

--- src/core/datasource.core.ts
+++ src/core/datasource.core.ts
@@ -1,6 +1,7 @@
-import { Utils, Types, Core } from '..';
+import { Utils, Types, Core, Booter, ORM } from '..';
 
 export abstract class Datasource {
+	protected mapper = Booter.getInstance().get(ORM.Mappers.Model.key());
 	constructor(private _config: any) { }
 	get config() { return this._config; }
 	static getName() {
@@ -9,9 +10,9 @@ export abstract class Datasource {
 	static getConfigFilename() {
 		return Utils.Reflector.getMetadata(Types.Bindings.Datasource.CONFIG_FILENAME_KEY.toString(), this);
 	}
-	abstract find<T extends Core.Model<T>>(uri: string, filter: any): Promise<T[]>;
-	abstract findById<T extends Core.Model<T>>(uri: string, id: string | number): Promise<T>;
+	abstract find<T extends Core.Model<T>>(uri: string, filter: Types.Thirdparty.Filter<T>): Promise<T[]>;
+	abstract findById<T extends Core.Model<T>>(uri: string, id: string | number): Promise<T | undefined>;
 	abstract create<T extends Core.Model<T>>(uri: string, model: T): Promise<T>;
-	abstract delete(uri: string, idOrFilter: any): Promise<boolean>;
-	abstract update<T extends Core.Model<T>>(uri: string, model: T, filter?: any): Promise<number>;
+	abstract delete<T extends Core.Model<T>>(uri: string, idOrFilter: string | Types.Thirdparty.Where<T>): Promise<boolean>;
+	abstract update<T extends Core.Model<T>>(uri: string, model: T, filter?: string | Types.Thirdparty.Where<T>): Promise<number>;
 }

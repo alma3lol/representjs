f2df44dd577e1d854b0b411c48d3f473617e52d4
Omar Almalol <omar.alma3lol@gmail.com>
2020-08-23 20:12:18 +0200

    feat(utils): refactors core's serialize util function

--- src/utils/core.util.ts
+++ src/utils/core.util.ts
@@ -1,5 +1,9 @@
 import { Class } from '../types/common.types'
 
 export namespace Core {
-	export const serialize = <T>(data: Partial<T>, cls: Class<T>): T => new cls(data);
+	export const serialize = <T>(cls: Class<T>, data?: Partial<T>): T => {
+		const obj = new cls();
+		Object.assign(obj, data);
+		return obj;
+	}
 }

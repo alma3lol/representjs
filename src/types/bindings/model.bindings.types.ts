11a764f0410263a4fec60921f58e8a70332050de
Omar Almalol <omar.alma3lol@gmail.com>
2020-08-23 16:13:54 +0200

    feat(types): removes name key & adds properties key

--- src/types/bindings/model.bindings.types.ts
+++ src/types/bindings/model.bindings.types.ts
@@ -2,8 +2,9 @@ import { Binding, ORM } from '../../core';
 
 export namespace Model {
 	export const TABLE_KEY = new Binding.Key<string>("MODEL.TABLE");
-	export const NAME_KEY = new Binding.Key<string>("MODEL.NAME");
 	export const URI_KEY = new Binding.Key<string>("MODEL.URI");
 	export const ID_PROPERTY_KEY = new Binding.Key<string>("MODEL.ID-PROPERTY");
-	export const RELATIONS_KEY = new Binding.Key<ORM.Relation<any>[]>("MODEL.RELATION");
+	export const ID_PROPERTY_TYPE_KEY = new Binding.Key<any>("MODEL.ID-PROPERTY-TYPE");
+	export const RELATIONS_KEY = new Binding.Key<ORM.Relation<any>[]>("MODEL.RELATIONS");
+	export const PROPERTIES_KEY = new Binding.Key<string[]>("MODEL.PROPERTIES");
 }

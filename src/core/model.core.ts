ca75277ba8d76b94c9e6fa2177c71fa2a1fa26e0
Omar Almalol <omar.alma3lol@gmail.com>
2020-08-23 16:07:36 +0200

    feat(core): adds static metadata registery & replaces reflector usage with static functions

--- src/core/model.core.ts
+++ src/core/model.core.ts
@@ -1,23 +1,42 @@
-import { Utils, Types } from '..';
+import { Types, Core } from '..';
 import { Serializable } from './serializable.core';
 
 export class Model<T extends Model<T>> extends Serializable {
+	protected static _registery: Map<string, { [name: string]: Core.Binding.Value<any> }> = new Map();
+	static bind<T>(name: string, key: Core.Binding.Key<T> | string): Core.Binding.Value<T> {
+		const binding = new Core.Binding.Value(key);
+		const data = this._registery.get(name) ?? {};
+		data[key.toString()] = binding;
+		this._registery.set(name, data);
+		return binding;
+	}
+	static get(name: string): { [name: string]: Core.Binding.Value<any> } {
+		return this._registery.get(name) as any;
+	}
+	static getSubKey<T>(name: string, key: Core.Binding.Key<T> | string): Core.Binding.Value<T> | undefined {
+		const data = this._registery.get(name) as any;
+		if (data === undefined) return undefined;
+		return data[key.toString()];
+	}
 	[name: string]: any;
 	constructor(data?: Partial<T>) {
 		super();
 		Object.assign(this, data)
 	}
 	get ID() {
-		return this[Utils.Reflector.getMetadata(Types.Bindings.Model.ID_PROPERTY_KEY.toString(), this)];
+		return this[Model.getSubKey(this.Name, Types.Bindings.Model.ID_PROPERTY_KEY)?.value ?? "id"];
 	}
 	static getName() {
-		return Utils.Reflector.getMetadata(Types.Bindings.Model.NAME_KEY.toString(), this);
+		return this.name;
 	}
-	static getTable() {
-		return Utils.Reflector.getMetadata(Types.Bindings.Model.TABLE_KEY.toString(), this);
+	get Name() {
+		return this.constructor.name;
 	}
-	static getURI() {
-		return Utils.Reflector.getMetadata(Types.Bindings.Model.URI_KEY.toString(), this);
+	get Table() {
+		return Model.getSubKey(this.Name, Types.Bindings.Model.TABLE_KEY)?.value;
+	}
+	get URI() {
+		return Model.getSubKey(this.Name, Types.Bindings.Model.URI_KEY)?.value;
 	}
 	static serialize<T extends Model<T>>(data: Partial<T>) {
 		return this.prototype.serialize(data, this);

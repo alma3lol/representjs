6ad200b475c43c8a434976139b0fd57173f90ca4
Omar Almalol <omar.alma3lol@gmail.com>
2020-11-12 04:16:25 +0200

    refactor(orm): renames toModel to resolveValue & uses config.value instead of target

--- src/orm/belongsto.orm.ts
+++ src/orm/belongsto.orm.ts
@@ -3,5 +3,5 @@ import { ORM, Model } from '../core';
 
 export class BelongsTo<T extends Model<T>> extends ORM.Relation<T> implements Types.CoreType.ORM.OneModelRelation<T> {
 	type: Types.CoreType.ORM.RelationType = "BelongsTo";
-	toModel = (target: T) => this.mapper.find(this.config.cls, this.config.key, target.ID_PROPERTY);
+	resolveValue = () => this.mapper.find(this.config.cls, this.config.key, this.config.value);
 }

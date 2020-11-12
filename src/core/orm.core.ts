f4c0ed32e024a207e7746c0625b69ccb048ae330
Omar Almalol <omar.alma3lol@gmail.com>
2020-11-12 04:06:53 +0200

    feat(core): adds update config function

--- src/core/orm.core.ts
+++ src/core/orm.core.ts
@@ -9,11 +9,19 @@ export namespace ORM {
 	export class Relation<T extends Model<T>> {
 		protected mapper = Booter.getInstance().get(Bindings.Core.MODEL_MAPPER_KEY);
 		constructor(
-			protected readonly config: CoreType.ORM.RelationConfig<T>
+			protected config: CoreType.ORM.RelationConfig<T>
 		) { }
 		/**
 		 * Relation type
 		 */
 		type: CoreType.ORM.RelationType;
+		/**
+		 * Updates config
+		 * 
+		 * ONLY USE WHEN REQUIRED
+		 */
+		updateConfig(config: CoreType.ORM.RelationConfig<T>) {
+			this.config = config;
+		}
 	}
 }

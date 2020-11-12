f0f65b0eac8e307c0ba76b79bafdc8c0937edacd
Omar Almalol <omar.alma3lol@gmail.com>
2020-11-12 04:17:25 +0200

    tests(decorators): adds testing for set property value after applying decorator

--- src/tests/decorators/orm.test.ts
+++ src/tests/decorators/orm.test.ts
@@ -4,6 +4,7 @@ import { HasMany } from '../../orm';
 describe('Decorators', () => {
 	describe('ORM', () => {
 		class TestModel extends Core.Model<TestModel> {
+			@Decorators.Model.property()
 			id: string;
 		}
 		beforeAll(() => {
@@ -11,7 +12,7 @@ describe('Decorators', () => {
 			mapper.bind("123").to(new TestModel({ id: "123" }));
 		})
 		it('should inject a relation into model\'s metadata', () => {
-			class Test extends Core.Model<TestModel> {
+			class Test extends Core.Model<Test> {
 				@Decorators.Model.ID({
 					type: String,
 					default: "123"
@@ -29,14 +30,14 @@ describe('Decorators', () => {
 			expect(result.get("test")).toBeInstanceOf(ORM.HasOne);
 		});
 		it('should auto-resolve relation\'s value', () => {
-			class Test extends Core.Model<TestModel> {
+			class Test extends Core.Model<Test> {
 				@Decorators.Model.ID({
 					type: String,
 					default: "123"
 				})
 				id: string;
 
-				@Decorators.ORM.relation("HasOne", {
+				@Decorators.ORM.Relation.hasOne({
 					key: "id",
 					cls: TestModel
 				})
@@ -45,6 +46,26 @@ describe('Decorators', () => {
 			const testClass = new Test();
 			expect(testClass.test).toBeInstanceOf(TestModel);
 			expect(testClass.test.id).toBe("123");
+		});
+		it('should be able to set relational properties\' value', () => {
+			class Test extends Core.Model<Test> {
+				@Decorators.Model.ID({
+					type: String,
+					default: "123"
+				})
+				id: string;
+
+				@Decorators.ORM.Relation.belongsTo({
+					key: "id",
+					cls: TestModel
+				})
+				test: string;
+			}
+			const testClass = new Test({
+				test: "123"
+			});
+			expect(testClass.test).toBeInstanceOf(TestModel);
+			expect(JSON.parse(testClass.test.toString())).toEqual({ id: "123" });
 		});
 	});
 });

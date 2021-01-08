8d6ea9f283ddb2e74b27b763636008b90d718171
Omar Almalol <omar.alma3lol@gmail.com>
2021-01-08 02:02:22 +0200

    feat(cli/bin): adds models' directory existance check and creation

--- cli/bin/cli.js
+++ cli/bin/cli.js
@@ -2,7 +2,7 @@
 
 const { CLI } = require('../dist');
 const pkg = require('../package.json');
-const {existsSync} = require('fs');
+const {existsSync, mkdirSync} = require('fs');
 const path = require('path');
 const Chalk = require('chalk');
 const _ = require('lodash');
@@ -14,6 +14,7 @@ CLI.commander.version(pkg.version);
 
 if (require.main == module) {
 	CLI.commander.parse(process.argv);
+	if (!existsSync(CLI.commander.opts()['srcDir'])) mkdirSync(CLI.commander.opts()['srcDir']);
 	if (CLI.commander.opts()['dryRun']) CLI.gitter.dryRun = true;
 	if (CLI.commander.opts()['signoff']) CLI.gitter.signoff = CLI.commander.opts()['signoff'];
 	const PACKAGE_PATH = path.join(process.cwd(), CLI.commander.opts()['package']);
